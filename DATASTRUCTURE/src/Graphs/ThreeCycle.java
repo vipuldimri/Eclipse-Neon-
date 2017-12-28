0d6663929854ad1f3a3c196b326aad8a2b7c4aa7
vipuldimri <dimrivipul.vd@gmail.com>
2017-12-28 11:19:44 +0530

    three cycle graph assigment completed

--- DATASTRUCTURE/src/Graphs/ThreeCycle.java
+++ DATASTRUCTURE/src/Graphs/ThreeCycle.java
@@ -7,28 +7,69 @@ public class ThreeCycle
 	  {
 
 		  int arr[][]=new int[n][n];
-		  for(int i=0;i<m;i++)
-		  {
-				  arr[U[i]-1][V[i]-1]=1;
-				  arr[V[i]-1][U[i]-1]=1;
 		
+		 for(int i=0 ; i<m ; i++)
+		  {
+			  
+			  arr[U[i]-1][V[i]-1] =1;
+			  arr[V[i]-1][U[i]-1] =1;
 		  }
 		  
-		  for(int i=0;i<n;i++)
-				{
-					for(int j=0;j<n;j++)
-					{
-						
-						System.out.print(arr[i][j]+" ");
-					}
-					System.out.println();
-			    }
+		 
+//		 for(int i=0;i<arr.length;i++)
+//		 {
+//			 for(int j=0;j<arr.length;j++)
+//			 {
+//				 System.out.print(arr[i][j]+" ");
+//			 }
+//			 System.out.println();
+//		 }
+		  int count = 0;
 		  
-		  return 1;
+		 for(int i=0;i < n ; i++)
+		 {
+		
+			 for(int j=0; j<n ; j++)
+			 {
+				 
+				 
+				 if(arr[i][j] == 1 )
+				 {
+
+					 for(int k=0 ; k < n ; k++)
+					 {
+						 if(arr[j][k] == 1 &&  k!=i)
+						 {
+							if(arr[i][k] == 1)
+							{
+								System.out.println("here "+i+","+k);
+								count++;
+							}
+							 
+						 }
+					 }
+					 
+				 }
+				 
+			 }
+			 
+			 
+			 
+			 
+		 }
+		 
+		 System.out.println("count is "+count);
+		  return count / 6;
 		  
-		  
+		  /*
+		   * 5
+14
+5 5 1 1 1 2 2 2 3 3 4 4 4 4
+4 2 2 3 4 1 4 5 1 4 1 2 3 5
+		   */
 		  
 	  }
+	  
 	public static void main(String[] args)
 	{
 		Scanner sc =new Scanner(System.in);
@@ -36,18 +77,17 @@ public class ThreeCycle
 		int m=sc.nextInt();
 		
 		int U[]=new int[m];
-		for(int i=0;i<U.length;i++)
+		for(int i=0;i<m;i++)
 		{
 			U[i]=sc.nextInt(); 
 		}
 		int V[]=new int[m];
-		for(int i=0;i<V.length;i++)
+		for(int i=0;i<m;i++)
 		{
-			U[i]=sc.nextInt();	
+			V[i]=sc.nextInt();	
 		}
 		
-		//System.out.println("abhe solve krna h ye ");
-		solve(n,m,U,V);
+		System.out.println(	solve(n,m,U,V));
 		
 	}
 
